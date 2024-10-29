37d4cbff6c5c90daca5f610e41076fd6d621a078
jesiel.carlos <jesielcarlos17@gmail.com>
2024-10-28 23:09:32 -0300

    Testando a comunicação

--- server_project/core/models.py
+++ server_project/core/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class CustonUsers(User):
+class CustomUsers(User):
     is_activated = models.BooleanField(
         default=True
     )
--- server_project/core/repositories/user_repository.py
+++ server_project/core/repositories/user_repository.py
@@ -1,19 +1,19 @@
-from core.models import CustonUsers
+from core.models import CustomUsers
 
 
 class UserRepository:
     def get_user_by_id(self, user_id: int):
-        user = CustonUsers.objects.filter(user_id=user_id).first()
+        user = CustomUsers.objects.filter(user_id=user_id).first()
 
         return user
     
     def get_all_users(self):
-        users = CustonUsers.objects.filter(is_active=True)
+        users = CustomUsers.objects.filter(is_active=True)
 
         return users
 
     def create_user(self, name: str, email: str, password: str):
-        user = CustonUsers.objects.create(
+        user = CustomUsers.objects.create(
             username=name,
             email=email
         )
